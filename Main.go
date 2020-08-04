83d7a4c8ef59f64be8380a8d92113807d0ac8282
mnlwldr <m.wildauer@gmail.com>
2020-08-04 16:19:56 +0200

    Inital

---
+++ Main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/url"
+	"strconv"
+
+	"github.com/ChimeraCoder/anaconda"
+)
+
+func initAnaconda() *anaconda.TwitterApi {
+	return anaconda.NewTwitterApiWithCredentials(
+		"",
+		"",
+		"",
+		"")
+}
+
+func main() {
+
+	api := initAnaconda()
+
+	params := url.Values{}
+	params.Set("count", "200")
+	params.Set("exclude_replies", "false")
+	params.Set("include_rts", "false")
+	params.Set("trim_user", "false")
+
+	var cursor int64 = -1 // Initial value, which is the first page
+	for cursor != 0 {
+		params.Set("cursor", strconv.FormatInt(cursor, 10))
+		tweets, err := api.GetUserTimeline(params)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, tweet := range tweets {
+			if tweet.FavoriteCount == 0 && tweet.RetweetCount == 0  {
+				api.DeleteTweet(tweet.Id, true)
+			}
+		}
+		cursor = 0
+	}
+}
